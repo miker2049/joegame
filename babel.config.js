0424ee867bdd2e8f4f63928fbb9430498604ff8b
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-03-30 10:22:36 -0400

    clean up

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "presets": [["@babel/preset-env", { "targets": "defaults", shippedProposals: true }],'@babel/preset-typescript'],
-  "plugins": ["@babel/plugin-proposal-object-rest-spread","@babel/plugin-transform-runtime"],
-  "sourceType": "unambiguous"
-}
