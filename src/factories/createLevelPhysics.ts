029ad692c589d28b98da2691808956932557a970
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-04-13 20:41:34 -0400

    physics bug fix

--- src/factories/createLevelPhysics.ts
+++ src/factories/createLevelPhysics.ts
@@ -1,28 +1,28 @@
-import {ILevelComponents} from '../ILevel'
+import { ILevelComponents } from '../ILevel'
 import touchToDir from '../utils/touchToDir'
 
 
-export default function(level: ILevelComponents): void{
-    level.scene.physics.world.addCollider(level.npcs,level.npcs,(npc1,npc2)=>{
-        level.machineRegisty.sendTo(npc1.name+'_machine',{type:'BUMP', sprite: npc2})
-        level.machineRegisty.sendTo(npc2.name+'_machine',{type:'BUMP', sprite: npc1})
+export default function(level: ILevelComponents): void {
+    level.scene.physics.world.addCollider(level.npcs, level.npcs, (npc1, npc2) => {
+        level.machineRegistry.sendTo(npc1.name + '_machine', { type: 'BUMP', sprite: npc2 })
+        level.machineRegistry.sendTo(npc2.name + '_machine', { type: 'BUMP', sprite: npc1 })
     })
     if (level.player) {
-        level.scene.physics.world.addCollider(level.player,level.npcs,(npc1,npc2)=>{
-            level.machineRegisty.sendTo('player_machine',{type:'BUMP', dir: touchToDir(npc2.body.touching)})
-            level.machineRegisty.sendTo(npc2.name+'_machine',{type:'BUMP', sprite: npc1})
+        level.scene.physics.world.addCollider(level.player, level.npcs, (npc1, npc2) => {
+            level.machineRegistry.sendTo('player_machine', { type: 'BUMP', dir: touchToDir(npc2.body.touching) })
+            level.machineRegistry.sendTo(npc2.name + '_machine', { type: 'BUMP', sprite: npc1 })
         })
     }
 
     level.map.layers.forEach((l) => {
         level.scene.physics.world.addCollider(l.tilemapLayer, level.npcs, (npc1, npc2) => {
-            level.machineRegisty.sendTo(npc1.name + '_machine', { type: 'BUMP', sprite: npc2 })
-            level.machineRegisty.sendTo(npc2.name + '_machine', { type: 'BUMP', sprite: npc1 })
+            level.machineRegistry.sendTo(npc1.name + '_machine', { type: 'BUMP', sprite: npc2 })
+            level.machineRegistry.sendTo(npc2.name + '_machine', { type: 'BUMP', sprite: npc1 })
         })
         if (level.player) {
             level.scene.physics.world.addCollider(l.tilemapLayer, level.player, (npc1, npc2) => {
-                level.machineRegisty.sendTo(npc1.name + '_machine', { type: 'BUMP', sprite: npc2 })
-                level.machineRegisty.sendTo('player_machine', { type: 'BUMP', sprite: npc1 })
+                level.machineRegistry.sendTo(npc1.name + '_machine', { type: 'BUMP', sprite: npc2 })
+                level.machineRegistry.sendTo('player_machine', { type: 'BUMP', sprite: npc1 })
             })
         }
 
