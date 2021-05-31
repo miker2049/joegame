09b704ae81463cc87d4bfb9776b032fb94783b78
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-05-31 11:03:37 -0400

    Site: pulling over stuff in prep for site
    pwa, favicon, eleventy config, deps

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const CleanCSS = require("clean-css");
+const UglifyJS = require("uglify-js");
+const path = require("path")
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("assets")
+
+    eleventyConfig.addPassthroughCopy({ "node_modules/gravisPats/gravis": "gravis" })
+    eleventyConfig.addPassthroughCopy({ "node_modules/gravisPats/gravis-Standard": "gravis-Standard" })
+    eleventyConfig.addPassthroughCopy({ "node_modules/gravisPats/gravis.cfg": "gravis.cfg" })
+
+    eleventyConfig.addPassthroughCopy({ "node_modules/joegame-lib/dist/joegame-lib.min.js": "joegame-lib.min.js" })
+
+    eleventyConfig.addPassthroughCopy({ "node_modules/timidity-wasm/dist/worklet-bundle.js": "worklet-bundle.js" })
+    eleventyConfig.addPassthroughCopy({ "node_modules/joegame-twitter-dialogue-scraper/convos": "assets/convos" })
+
+
+    eleventyConfig.addPassthroughCopy({ "pwa": "/" })
+    eleventyConfig.addPassthroughCopy("favicon.ico")
+    // eleventyConfig.addPassthroughCopy("pwa/*")
+
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    eleventyConfig.addFilter("jsmin", function(code) {
+        return new UglifyJS.minify(code).code;
+    });
+
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+};
