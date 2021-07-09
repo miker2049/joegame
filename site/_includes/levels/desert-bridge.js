8ba3610213aca0909dc2a246ae031c8698326ed5
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-07-09 07:57:38 -0400

    Game: desert bridge work

--- site/_includes/levels/desert-bridge.js
+++ site/_includes/levels/desert-bridge.js
@@ -5,11 +5,11 @@ joegameLib.loadMap(
     'assets/maps/desert_bridge.json',
     '/',
     '/assets/data.csv',
-    { x: 480, y: 448}, {objectLayers: ['canyon', 'aboveObjects'] })
+    { x: 480, y: 448}, {objectLayers: ['canyon', 'aboveObjects', 'swirls'] })
     .then(out => {
         var fac = out[1]
         var level = out[0]
-
+        joegameLib.runCinematicNode(level,'Start','assets/dialogues/swirl-reflection.json')
         level.scene.input.keyboard.on('keydown-SPACE', () => {
             joegameLib.sparkleCircle(level.player)
         })
