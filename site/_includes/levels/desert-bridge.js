71b782b8b8182659014fcb5c666f16eb79fc5951
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-07-15 16:21:09 -0400

    Game: final desert scene pretty much

--- site/_includes/levels/desert-bridge.js
+++ site/_includes/levels/desert-bridge.js
@@ -9,9 +9,16 @@ joegameLib.loadMap(
     .then(out => {
         var fac = out[1]
         var level = out[0]
-        joegameLib.runCinematicNode(level,'Start','assets/dialogues/swirl-reflection.json')
-        level.scene.input.keyboard.on('keydown-SPACE', () => {
-            joegameLib.sparkleCircle(level.player)
+        var context = level.scene.sound.context
+        joegameLib.playMIDIFile('/assets/audio/midi/joegame-swirl-slow2.mid', context).then(plyr => {
+            console.log(plyr)
+
+            console.log('***********************************8')
+            level.scene.input.keyboard.on('keydown-SPACE', () => {
+                joegameLib.sparkleCircle(level.player)
+                plyr.play()
+            })
         })
+        joegameLib.runCinematicNode(level, 'Start', 'assets/dialogues/swirl-reflection.json')
     })
     .catch(err => console.log(err))
