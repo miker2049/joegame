4e2ef9fda18fec1b67816e7f86487bb4fea72116
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-08-01 01:49:07 -0400

    Lib: fix tile from point

--- src/utils/getTileFromPoint.ts
+++ src/utils/getTileFromPoint.ts
@@ -1,7 +1,7 @@
 import 'phaser'
+import {AssuredVec2} from '../joegameTypes'
 
-export default function(go: Phaser.Types.Math.Vector2Like, tilesize: number): Phaser.Types.Math.Vector2Like | undefined {
-    if (go.x === undefined || go.y === undefined) return
+export default function(go: AssuredVec2, tilesize: number): AssuredVec2 {
     const x_ = (Math.floor((go.x) / tilesize) * tilesize)
     const y_ = (Math.floor((go.y) / tilesize) * tilesize)
     return { x: x_ / tilesize, y: (y_ / tilesize) }
