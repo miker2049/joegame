891ff993c9e71e1eeb696df085ccf91c14ddc205
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-08-17 18:10:56 -0400

    Repo: asset doctor updates

--- scripts/asset-doctor.js
+++ scripts/asset-doctor.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk');
 const fs = require('fs')
+const fsp = require('fs/promises')
+const glob = require('glob')
 
 const sectionLog = (str) => console.log(chalk.black.bgBlue(str))
 function exists(path) {
@@ -18,13 +20,39 @@ function exists(path) {
 
 sectionLog("Compare convo manifest with actually existing files")
 
-const mani = JSON.parse(fs.readFileSync('assets/convos/convo-manifest.json'))
+const mani = JSON.parse(fs.readFileSync('assets/tweet-convos/convo-manifest.json'))
 
 let missing = []
 mani.forEach(f => {
-    const ex = exists('assets/convos/' + f)
+    const ex = exists('assets/tweet-convos/' + f)
     if (!ex) {
         console.log(chalk.red(f, 'is missing from the convos folder but present in the manifest'))
     }
 });
 
+function checkMapFileAssets (instr) {
+    let parse = JSON.parse(instr)
+    // console.log(parse.tilesets.images ?? undefined)
+    parse.tilesets.forEach(item => {
+        if(item.image){
+            console.log(item.image)
+        } else {
+            item.tiles.forEach(ti => console.log(ti.image) );
+        }
+    });
+}
+
+
+glob("assets/maps/*.json", function(er,files) {
+
+    Promise.all(
+        files.map((item, i, arr) => {
+            return fsp.readFile(item, {encoding: "utf-8"})
+        })
+    ).then((files) => {
+            // console.log(f)
+        files.forEach((it)=>checkMapFileAssets(it))
+    }).then((f)=>{
+        // console.log(f)
+    })
+})
