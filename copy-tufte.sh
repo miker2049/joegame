820d8bf4290176f37501bee4aa95a9fcba90d172
Mike Russo <6748794-beepbooptheory@users.noreply.gitlab.com>
2022-04-12 16:21:00 -0400

    WIP

--- copy-tufte.sh
+++ copy-tufte.sh
@@ -3,7 +3,7 @@
 SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd $SCRIPT_RELATIVE_DIR
 
-yarn run minify site/tufte.css > public/tufte.min.css
+node_modules/.bin/minify site/tufte.css > public/tufte.min.css
 echo "minified, copied tufte.css"
 cp -rf node_modules/tufte-css/et-book public/
 echo "minified, copied tufte fonts"
