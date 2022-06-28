f0ff1f8bf2684f3010064461c1f62f76c7d2cbd5
Mike Russo <russomichaelb@gmail.com>
2022-06-28 01:39:50 -0400

    add map-explorer app

---
+++ apps/map-explorer/src/app.tsx
@@ -0,0 +1,26 @@
+
+export function App() {
+    return (
+        <div class="app-container box">
+            <SideBar/>
+        </div>
+    )
+}
+export function SideBar() {
+    return (
+        <>
+        <div class='sidebar-container'>
+
+        </div>
+        <div class='main-container'>
+
+        <div class='submain-container'>
+
+        </div>
+        <div class='submain-container'>
+
+        </div>
+        </div>
+        </>
+    )
+}
