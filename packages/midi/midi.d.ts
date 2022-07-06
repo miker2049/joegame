d7a4f81c7ac50a59eef303bfd82eb913e30011a9
Mike Russo <6748794-beepbooptheory@users.noreply.gitlab.com>
2022-07-06 14:25:26 -0400

    clean up old midi files

--- packages/midi/midi.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'midi' {
-    export function createSynth(ctx: AudioContext, workletURL: string): Promise<AudioWorkletNode>
-    export function createWithFont(ctx: AudioContext, workletURL: string, sfURL: string): Promise<AudioWorkletNode>
-    export function loadFont(node: AudioWorkletNode, sfURL: string): Promise<void>
-}
