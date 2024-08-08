ec8fd0ccc2df8f0ce9add52af0808b50ddbe842d
Mike Russo <russomichaelb@gmail.com>
2024-08-08 10:50:52 -0600

    flappy-turd: shitty dynamic obstacles

--- packages/flappy-turd/obstacle.gd
+++ packages/flappy-turd/obstacle.gd
@@ -1,6 +1,6 @@
 extends Area2D
 @onready var collider = $CollisionShape2D
-
+@onready var spr = $Sprite2D
 
 var speed = 100
 var accel = 0.001
@@ -8,7 +8,13 @@ var accel = 0.001
 func _ready():
 	collider.shape = RectangleShape2D.new()
 	collider.shape.size = Vector2(20,randi_range(20,280))
+	_sync_shader_scale()
 
+func _sync_shader_scale():
+	spr.position = collider.position
+	spr.scale = collider.shape.size/spr.texture.get_size()
+	print(spr.scale)
+	spr.material.set_shader_parameter("scaleTransform", spr.scale)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,6 +26,7 @@ func _process(delta):
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	position.x = get_viewport().get_visible_rect().size.x
 	collider.shape.size = Vector2(20,randi_range(20,280))
+	_sync_shader_scale()
 	
 
 
